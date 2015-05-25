e437139bba03f560498f0aaa8908c5485109f7fc
Jamie Hall <jamie@kaggle.com>
2015-05-25 09:06:02 -0700

    Partway conversion to React/sass

--- www/public/js/reps.js
+++ www/public/js/reps.js
@@ -14,12 +14,12 @@ var RepsResultText = React.createClass({displayName: "RepsResultText",
         var nOthSeats = countSeats("OTH");
         return(
             React.createElement("div", {class: "repsforecasttext"}, 
-                React.createElement("h4", null, "Current Forecast Summary"), 
-                "The model's best guess sees the LNP government winning ", React.createElement("span", {class: "lnprepsnumber"}, nLnpSeats), "," + ' ' +
-                "the ALP ", React.createElement("span", {class: "alprepsnumber"}, nAlpSeats), "," + ' ' +
-                "the Greens ", React.createElement("span", {class: "grnrepsnumber"}, nGrnSeats), "," + ' ' +
-                "Palmer United with ", React.createElement("span", {class: "puprepsnumber"}, nPupSeats), " and" + ' ' +
-                "Independents/KAP/Others winning ", React.createElement("span", {class: "othrepsnumber"}, nOthSeats), "."
+                React.createElement("h4", null, "Summary"), 
+                "The model's best guess sees the LNP government winning ", React.createElement("span", {className: "repsSummaryNumber"}, nLnpSeats), "," + ' ' +
+                "the ALP ", React.createElement("span", {className: "repsSummaryNumber"}, nAlpSeats), "," + ' ' +
+                "the Greens ", React.createElement("span", {className: "repsSummaryNumber"}, nGrnSeats), "," + ' ' +
+                "Palmer United with ", React.createElement("span", {className: "repsSummaryNumber"}, nPupSeats), " and" + ' ' +
+                "Independents winning ", React.createElement("span", {className: "repsSummaryNumber"}, nOthSeats), "."
             )
         );
     }
@@ -39,13 +39,89 @@ var RepsSeatList = React.createClass({displayName: "RepsSeatList",
     }
 });
 
+var RepsSeatHeaderRow = React.createClass({displayName: "RepsSeatHeaderRow",
+    render: function() {
+        return(
+            React.createElement("div", {className: "seatHeaderRow"}, 
+                React.createElement("div", null, this.props.data.name), 
+                React.createElement("div", null, this.props.data.description)
+            )
+        );
+    }
+});
+
+var RepsSeatIncumbentRow = React.createClass({displayName: "RepsSeatIncumbentRow",
+    render: function() {
+        return(
+            React.createElement("div", {className: "seatIncumbentRow"}, 
+                React.createElement("div", {className: "seatState"}, this.props.data.state), 
+                React.createElement("div", {className: "seatIncumbentName"}, this.props.data.member), 
+                React.createElement("div", {className: "seatIncumbentParty"}, this.props.data.incumbentparty)
+            )
+        );
+    }
+});
+
+function roundVote(v){
+    return Math.round(v*10)/10;
+}
+
+var RepsSeatTwoppRow = React.createClass({displayName: "RepsSeatTwoppRow",
+    render: function() {
+        return(
+            React.createElement("div", {className: "seatTwoPPRow"}, 
+                React.createElement("div", {className: "seat2pp LNP"}, roundVote(this.props.data.lnp2pp)), 
+                React.createElement("div", {className: "seat2pp ALP"}, roundVote(this.props.data.alp2pp))
+            )
+        );
+    }
+});
+
+var RepsSeatPrimaryRow = React.createClass({displayName: "RepsSeatPrimaryRow",
+    render: function() {
+        return(
+            React.createElement("div", {className: "seatPrimaryRow"}, 
+                React.createElement("div", {className: "seatPrimary LNP"}, roundVote(this.props.data.lnpprimary)), 
+                React.createElement("div", {className: "seatPrimary ALP"}, roundVote(this.props.data.alpprimary)), 
+                React.createElement("div", {className: "seatPrimary GRN"}, roundVote(this.props.data.grnprimary)), 
+                React.createElement("div", {className: "seatPrimary PUP"}, roundVote(this.props.data.pupprimary)), 
+                React.createElement("div", {className: "seatPrimary OTH"}, roundVote(this.props.data.othprimary))
+            )
+        );
+    }
+});
+
+var RepsSeatExtraInfoRow = React.createClass({displayName: "RepsSeatExtraInfoRow",
+    render: function() {
+        var wikiLink = "http://en.wikipedia.org/wiki/Division_of_" + this.props.data.name;
+        return(
+            React.createElement("div", {className: "seatExtraInfo"}, 
+                React.createElement("div", {className: "seatWikiLink"}, 
+                    React.createElement("a", {href: wikiLink}, "Wiki")
+                )
+            )
+        );
+    }
+});
+
+function getTileCssClass(d){
+    var seatType = "Retain";
+    if(d.winner !== d.incumbentparty){
+        seatType = "Win";
+    }
+    return(d.winner.toLowerCase() + seatType);
+}
+
 var RepsSeat = React.createClass({displayName: "RepsSeat",
     render: function() {
-        var d = this.props.data;
+        var thisSeatClass = "seatTile " + getTileCssClass(this.props.data);
         return(
-            React.createElement("div", {class: "repsSeatTile"}, 
-                React.createElement("div", {class: "seatName"}, d.name), 
-                React.createElement("div", {class: "seatDescription"}, d.description)
+            React.createElement("div", {className: thisSeatClass}, 
+                React.createElement(RepsSeatHeaderRow, {data: this.props.data}), 
+                React.createElement(RepsSeatIncumbentRow, {data: this.props.data}), 
+                React.createElement(RepsSeatTwoppRow, {data: this.props.data}), 
+                React.createElement(RepsSeatPrimaryRow, {data: this.props.data}), 
+                React.createElement(RepsSeatExtraInfoRow, {data: this.props.data})
             )
         );
     }
---
+++ www/react/reps.jsx
@@ -0,0 +1,141 @@
+var RepsResultText = React.createClass({
+    render: function(){
+        var countSeats = (function(p){
+            return(Object.keys(this.props.resultList)
+                .filter(function(x) {
+                    return houseResults[x].winner == p;
+                })
+                .length);
+        }).bind(this);
+        var nAlpSeats = countSeats("ALP");
+        var nLnpSeats = countSeats("LNP");
+        var nGrnSeats = countSeats("GRN");
+        var nPupSeats = countSeats("PUP");
+        var nOthSeats = countSeats("OTH");
+        return(
+            <div class="repsforecasttext">
+                <h4>Summary</h4>
+                The model's best guess sees the LNP government winning <span className="repsSummaryNumber">{nLnpSeats}</span>,
+                the ALP <span className="repsSummaryNumber">{nAlpSeats}</span>,
+                the Greens <span className="repsSummaryNumber">{nGrnSeats}</span>,
+                Palmer United with <span className="repsSummaryNumber">{nPupSeats}</span> and
+                Independents winning <span className="repsSummaryNumber">{nOthSeats}</span>.
+            </div>
+        );
+    }
+});
+
+var RepsSeatList = React.createClass({
+    render: function() {
+        var seatNodes = [];
+        for(var seatName in this.props.data){
+            seatNodes.push( <RepsSeat data={this.props.data[seatName]} /> );
+        }
+        return (
+            <div className="repsSeatList">
+                {seatNodes}
+            </div>
+        );
+    }
+});
+
+var RepsSeatHeaderRow = React.createClass({
+    render: function() {
+        return(
+            <div className="seatHeaderRow">
+                <div>{this.props.data.name}</div>
+                <div>{this.props.data.description}</div>
+            </div>
+        );
+    }
+});
+
+var RepsSeatIncumbentRow = React.createClass({
+    render: function() {
+        return(
+            <div className="seatIncumbentRow">
+                <div className="seatState">{this.props.data.state}</div>
+                <div className="seatIncumbentName">{this.props.data.member}</div>
+                <div className="seatIncumbentParty">{this.props.data.incumbentparty}</div>
+            </div>
+        );
+    }
+});
+
+function roundVote(v){
+    return Math.round(v*10)/10;
+}
+
+var RepsSeatTwoppRow = React.createClass({
+    render: function() {
+        return(
+            <div className="seatTwoPPRow">
+                <div className="seat2pp LNP">{roundVote(this.props.data.lnp2pp)}</div>
+                <div className="seat2pp ALP">{roundVote(this.props.data.alp2pp)}</div>
+            </div>
+        );
+    }
+});
+
+var RepsSeatPrimaryRow = React.createClass({
+    render: function() {
+        return(
+            <div className="seatPrimaryRow">
+                <div className="seatPrimary LNP">{roundVote(this.props.data.lnpprimary)}</div>
+                <div className="seatPrimary ALP">{roundVote(this.props.data.alpprimary)}</div>
+                <div className="seatPrimary GRN">{roundVote(this.props.data.grnprimary)}</div>
+                <div className="seatPrimary PUP">{roundVote(this.props.data.pupprimary)}</div>
+                <div className="seatPrimary OTH">{roundVote(this.props.data.othprimary)}</div>
+            </div>
+        );
+    }
+});
+
+var RepsSeatExtraInfoRow = React.createClass({
+    render: function() {
+        var wikiLink = "http://en.wikipedia.org/wiki/Division_of_" + this.props.data.name;
+        return(
+            <div className="seatExtraInfo">
+                <div className="seatWikiLink">
+                    <a href={wikiLink}>Wiki</a>
+                </div>
+            </div>
+        );
+    }
+});
+
+function getTileCssClass(d){
+    var seatType = "Retain";
+    if(d.winner !== d.incumbentparty){
+        seatType = "Win";
+    }
+    return(d.winner.toLowerCase() + seatType);
+}
+
+var RepsSeat = React.createClass({
+    render: function() {
+        var thisSeatClass = "seatTile " + getTileCssClass(this.props.data);
+        return(
+            <div className={thisSeatClass}>
+                <RepsSeatHeaderRow data={this.props.data} />
+                <RepsSeatIncumbentRow data={this.props.data} />
+                <RepsSeatTwoppRow data={this.props.data} />
+                <RepsSeatPrimaryRow data={this.props.data} />
+                <RepsSeatExtraInfoRow data={this.props.data} />
+            </div>
+        );
+    }
+});
+
+var RepsSection = React.createClass({
+    render: function(){
+        return(
+            <div>
+            <RepsResultText resultList={this.props.data} />
+            <RepsSeatList data={this.props.data} />
+            </div>
+        );
+    }
+});
+
+React.render(<RepsSection data={houseResults} />, document.getElementById('repsApp'));
